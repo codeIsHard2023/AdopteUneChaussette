cd057b7b3cddc2afa6b57bb49d9cccc660fcce14
Vladyslav TEZYK <vlad.tezyk.pro@gmail.com>
2023-12-04 19:05:41 +0100

    carousel new draft

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // const card = document.getElementsByClassName("card")
 // console.log(card)
-// const slideRight = document.querySelector(".slide-right")
-// console.log(slideRight)
+// const slideLeft = document.querySelector(".btn-slide-left")
+// console.log(slideLeft)
 
-// slideRight.addEventListener("click", function (event) {
-//     card.classList.toggle() = ".animation-right"
+// slideLeft.addEventListener("click", () => {
+//     card.classList.toggle("animation-left")
 //   });
+
+const carousel = document.querySelector(".carousel")
+const arrowBtns = document.querySelectorAll(".carousel-container i");
+// console.log(arrowBtns)
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        // console.log(btn.id);
+        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    })
+});
