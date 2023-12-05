a8d2a84121ff1e26c19b77cdc8e07c641725472a
RetrozDev <ryanbarbozapro@gmail.com>
2023-12-05 16:05:48 +0100

    Added possibility to change user pictures (reset on page reload)

--- script.js
+++ script.js
@@ -6,3 +6,39 @@
 // slideRight.addEventListener("click", function (event) {
 //     card.classList.toggle() = ".animation-right"
 //   });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const userGalleryPictures = document.querySelectorAll('.myPic');
+  
+    userGalleryPictures.forEach((pic) => {
+      pic.addEventListener('click', () => {
+        userGalleryPictures.forEach((p) => p.classList.remove('selected'));
+        pic.classList.add('selected');
+        document.getElementById('fileInput').click();
+      });
+    });
+  
+    document.getElementById('fileInput').addEventListener('change', handleFileChange);
+  });
+  
+  function handleFileChange() {
+    const fileInput = document.getElementById('fileInput');
+    const userGalleryPictures = document.querySelectorAll('.myPic');
+  
+    if (fileInput.files && fileInput.files[0]) {
+      const reader = new FileReader();
+  
+      reader.onload = (e) => {
+        userGalleryPictures.forEach((pic) => {
+          if (pic.classList.contains('selected')) {
+            pic.style.backgroundImage = `url('${e.target.result}')`;
+            pic.classList.remove('selected');
+          }
+        });
+      };
+  
+      reader.readAsDataURL(fileInput.files[0]);
+    }
+  }
+  
+  
