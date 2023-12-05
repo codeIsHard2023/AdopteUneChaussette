11e2d61bd6c903f8d15f72078263428916a0cd0a
Vladyslav TEZYK <vlad.tezyk.pro@gmail.com>
2023-12-05 18:31:40 +0100

    responsive styling modified

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const carousel = document.querySelector(".carousel")
 const arrowBtns = document.querySelectorAll(".carousel-container i");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const firstCardHeight = carousel.querySelector(".card").offsetHeight;
 
 //Add event listener for the arrow button left and right 
 arrowBtns.forEach(btn => {
@@ -9,6 +10,14 @@ arrowBtns.forEach(btn => {
         carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
     })
 });
+
+// Add event listener for the arrow button up and down 
+// arrowBtns.forEach(btn => {
+//     btn.addEventListener("click", () => {
+//         // console.log(btn.id);
+//         carousel.scrollTop += btn.id === "up" ? -firstCardHeight : firstCardHeight;
+//     })
+// });
 
 const carouselChildren = [...carousel.children]
 
