c4a91856cdaffec37deb27c34f60c4275d46a8bf
Vladyslav TEZYK <vlad.tezyk.pro@gmail.com>
2023-12-05 11:53:54 +0100

    carousel modified : information added for each person card; animations on card hover; changes integrated into index.html and style.css; need to fix a bug with arrow visual aspect

--- script.js
+++ script.js
@@ -9,11 +9,28 @@
 
 const carousel = document.querySelector(".carousel")
 const arrowBtns = document.querySelectorAll(".carousel-container i");
-// console.log(arrowBtns)
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+
+//Add event listener for the arrow button left and right 
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         // console.log(btn.id);
         carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
     })
 });
+
+
+// const carouselChildren = [...carousel.children]
+
+// //Get the number of cards that can fit in the carousel at once
+// let cardPerView = Math.round(carousel.offsetWidth /firstCardWidth)
+
+// //Insert copies of the last few cards to beginning of carousel for infinite scrolling
+// carouselChildren.slice(-cardPerView).reverse().forEach( card => {
+//     carousel.insertAdjacentHTML("afterbegin", card.outerHTML)
+// })
+
+// //Insert copies of the first few cards to beginning of carousel for infinite scrolling
+// carouselChildren.slice(0,cardPerView).forEach( card => {
+//     carousel.insertAdjacentHTML("beforeend", card.outerHTML)
+// })
