f91c3f01eddace48c7b573ea7ebd3bfb6f2fec28
RetrozDev <ryanbarbozapro@gmail.com>
2023-12-05 12:03:00 +0100

    Added js for login and array with username and password

--- script.js
+++ script.js
@@ -6,3 +6,22 @@
 // slideRight.addEventListener("click", function (event) {
 //     card.classList.toggle() = ".animation-right"
 //   });
+
+
+// JS for login
+
+    const usernames = ["user1", "user2", "user3"];
+    const passwords = ["mdp1", "mdp2", "mdp3"];
+
+    function login() {
+        const usernameInput = document.getElementById("username").value;
+        const passwordInput = document.getElementById("password").value;
+
+        const index = usernames.indexOf(usernameInput);
+        if (index !== -1 && passwords[index] === passwordInput) {
+            document.getElementById("errorMessage").innerText = "Connexion réussie!";
+            window.location.href = "index.html";
+        } else {
+            document.getElementById("errorMessage").innerText = "Identifiants incorrects. Veuillez réessayer.";
+        }
+    }
