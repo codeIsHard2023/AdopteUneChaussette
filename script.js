284a8a148fb464082d4d933cd19f2637d01838c8
Vladyslav TEZYK <vlad.tezyk.pro@gmail.com>
2023-12-05 16:22:01 +0100

    infintite scrolling added to carousel; some styling modified

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ arrowBtns.forEach(btn => {
         carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
     })
 });
+
+const carouselChildren = [...carousel.children]
+
+//Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth)
+
+//Insert copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildren.slice(-cardPerView).reverse().forEach( card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML)
+})
+
+//Insert copies of the first few cards to beginning of carousel for infinite scrolling
+carouselChildren.slice(0,cardPerView).forEach( card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML)
+})
+
+const infiteScroll = () => {
+    //If the carousel is at the beginning, scroll to the end
+    if(carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }
+    //If the carousel is at the end, scroll to the beginning
+    else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+}
+carousel.addEventListener("scroll", infiteScroll)
