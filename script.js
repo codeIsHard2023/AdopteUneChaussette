28d94839548bc30a48ce6ccde7d94252e9cb1ebb
RetrozDev <ryanbarbozapro@gmail.com>
2023-12-06 16:20:28 +0100

    Added modal for gallery

--- script.js
+++ script.js
@@ -6,3 +6,27 @@
 // slideRight.addEventListener("click", function (event) {
 //     card.classList.toggle() = ".animation-right"
 //   });
+
+
+function openModal(clickedElement) {
+    const modal = document.getElementById("myModal");
+    const modalImg = document.getElementById("modalImage");
+  
+    modal.style.display = "flex";
+  
+    // Récupère le style de fond directement depuis le CSS de l'élément cliqué
+    const backgroundImage = getComputedStyle(clickedElement).backgroundImage;
+    modalImg.style.backgroundImage = backgroundImage;
+  }
+  
+  function closeModal() {
+    const modal = document.getElementById("myModal");
+  
+    modal.style.display = "none";
+  }
+  
+  
+  
+  
+  
+  
